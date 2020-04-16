ebefce21bd5eb7b407d0da98d4ada37bbd90a2af
Eridan Vilasboas <eridanvilas@gmail.com>
2020-04-15 21:51:18 -0300

    Estruturar rostas, configurar css das novas rotas;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Main from './pages/main';
+import Routes from './routes';
 import Header from './components/Header';
 import './styles.css';
 
@@ -7,7 +7,7 @@ import './styles.css';
 const App = () => (
   <div className="App">
     <Header/>
-    <Main/>
+    <Routes/>
   </div>
 )
 
